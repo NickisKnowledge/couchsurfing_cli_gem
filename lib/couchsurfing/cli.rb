7fb91dda04ff31d54c5fbce07bcc526f37103dad
NickisKnowledge <lifechangingstuff@gmail.com>
2017-08-04 18:19:17 -0500

    WIP; Added tmp user interface for continents selection.

--- lib/couchsurfing/cli.rb
+++ lib/couchsurfing/cli.rb
@@ -1,6 +1,20 @@
 class Couchsurfing::CLI
 
   def start
-    puts "What continent would you like to visit?"
+    get_continents
   end
+
+  def get_continents
+    puts "Select the number with the continent you would like to visit:"
+    puts <<-DOC.gsub /^\s*/, ''
+    1. Asia
+    2. Africa
+    3. Antarctica
+    4. Australia
+    5. Europe
+    6. North America
+    7. South America
+    DOC
+  end
+
 end
