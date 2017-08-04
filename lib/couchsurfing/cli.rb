72d7674108ada0028c333eece9c580b067007cad
NickisKnowledge <lifechangingstuff@gmail.com>
2017-08-04 18:27:23 -0500

    WIP; Added #exit & housing selection based on what will be the country - city.

--- lib/couchsurfing/cli.rb
+++ lib/couchsurfing/cli.rb
@@ -5,7 +5,7 @@ class Couchsurfing::CLI
   end
 
   def get_continents
-    puts "Select the number with the continent you would like to visit:"
+    puts "Select the number with the continent you would like to visit:\nType 'exit' to quite the program."
     puts <<-DOC.gsub /^\s*/, ''
     1. Asia
     2. Africa
@@ -15,6 +15,36 @@ class Couchsurfing::CLI
     6. North America
     7. South America
     DOC
+    input = gets.strip
+    get_city_and_state(input)
   end
+
+  def get_city_and_state(input)
+    if input == 'exit'
+      stop_surfing
+    else
+      puts "Select the number of the place for which you would like to view housing accommodations?"
+      case input
+      when '1'
+        puts "Choose one of these hoomes in [Asia]"
+      when '2'
+        puts "Choose one of these hoomes in [Africa]"
+      when '3'
+        puts "Choose one of these hoomes in [Antarctica]"
+      when '4'
+        puts "Choose one of these hoomes in [Australia]"
+      when '5'
+        puts "Choose one of these hoomes in [Europe]"
+      when '6'
+        puts "Choose one of these hoomes in [North America]"
+      when '7'
+        puts "Choose one of these hoomes in [South America]"
+      end
+    end
+  end
+
+    def stop_surfing
+      puts 'Enjoy your new temporary home.'
+    end
 
 end
