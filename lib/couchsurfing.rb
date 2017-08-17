37503263b38bbfafc60f32a89214d1aadade9dcc
NickisKnowledge <lifechangingstuff@gmail.com>
2017-08-16 22:04:01 -0500

    WIP; Created Scraper class and #scrape_location, #scrape_get_host_page & #scrape_host_page. Also, added file to environment file.

--- lib/couchsurfing.rb
+++ lib/couchsurfing.rb
@@ -1,3 +1,4 @@
 require_relative "./couchsurfing/version"
+require_relative 'couchsurfing/scrape'
 require_relative 'couchsurfing/place'
 require_relative 'couchsurfing/cli'
---
+++ lib/couchsurfing/scraper.rb
@@ -0,0 +1,18 @@
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
+class Couchsurfing::Scraper
+
+  def scrape_location
+
+  end
+
+  def scrape_get_host_page
+
+  end
+
+  def scrape_host_page
+
+  end
+end
