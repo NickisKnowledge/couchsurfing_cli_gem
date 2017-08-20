13383ae126f3f4642d3123cf6a672d73669ed1d7
NickisKnowledge <lifechangingstuff@gmail.com>
2017-08-19 23:30:49 -0500

    WIP; Finished Scraper code for scraping host page.

--- lib/couchsurfing/scraper.rb
+++ lib/couchsurfing/scraper.rb
@@ -51,9 +51,49 @@ class Couchsurfing::Scraper
     hosts
   end
 
-  def scrape_host_page
+  def scrape_host_page(host_url)
+    url = 'https://www.couchsurfing.com' + host_url
+    doc = Nokogiri::HTML(open(url))
+    # 'https://www.couchsurfing.com/people/susanne-fischer-2' # bad profile
+    # 'https://www.couchsurfing.com/people/damon-page' # no opening info
+    # 'https://www.couchsurfing.com/people/davor-medaric' # --reason 4 add code
+    # 'https://www.couchsurfing.com/people/mohcine-lar' # chatty-katy
+    # 'https://www.couchsurfing.com/people/davidsalib' # chatty-katy-> include
+    # 'https://www.couchsurfing.com/users/1005022110' # -- random Male
+    # 'https://www.couchsurfing.com/people/asmae-attif' # -- female
 
+    host = []
+    profile = {}
+
+    host_name = doc.css('div.profile-sidebar__user-info span a span').text
+    profile['name'] = host_name
+
+    z = doc.css('section:nth-child(3) div div div:nth-child(1) ul li:nth-child(3)').text
+    profile['age_sex'] = z
+
+    about_me_section = doc.css('div:nth-child(5) section:nth-child(4) div')
+
+    person = {}
+    profile['about_me'] = person
+
+   if about_me_section.css('p:nth-child(2)').text == ''
+     person['About Me'] = about_me_section.css('p:nth-child(1)').text
+   else
+     person['About Me'] = about_me_section.css('p:nth-child(1)').text + ": " \
+                          + about_me_section.css('p:nth-child(2)').text unless \
+                          about_me_section.css('p:nth-child(1)').text == ''
+   end
+
+    about_me_section.search('h2').each do |node|
+      if (node.next_element.text).include?('COUCHSURFING')
+        person[node.text] = node.next_element.text + ': ' + node.next_element.next_element.text
+      else
+        person[node.text] = node.next_element.text if node.next_element.text != ''
+      end
+    end
+    host << profile
   end
+
 end
 
  # x = Couchsurfing::Scraper.new
