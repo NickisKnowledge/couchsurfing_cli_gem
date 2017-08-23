b082410a1c60785b49197c25f150bc589a6bff03
NickisKnowledge <lifechangingstuff@gmail.com>
2017-08-22 20:24:11 -0500

    Final refactor of Scraper class. Added class comment, fixed indentation issues, deleted unnecessary comments & spaces. Changed 'if' in else clause in about_me_section loop to 'unless'.

--- lib/couchsurfing/scraper.rb
+++ lib/couchsurfing/scraper.rb
@@ -2,11 +2,11 @@ require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
+# Scrapes 'couchsurfing.com'
 class Couchsurfing::Scraper
-
   def scrape_locations
     doc = Nokogiri::HTML(open('https://www.couchsurfing.com/places'))
-          structure = doc.css('#section_places div:nth-child(4) div div' \
+    structure = doc.css('#section_places div:nth-child(4) div div' \
                               ' div.pure-u-1.pure-u-md-15-24 div')
     regions = structure.css('.cs-sitemap-region')
 
@@ -54,13 +54,6 @@ class Couchsurfing::Scraper
   def scrape_host_page(host_url)
     url = 'https://www.couchsurfing.com' + host_url
     doc = Nokogiri::HTML(open(url))
-    # 'https://www.couchsurfing.com/people/susanne-fischer-2' # bad profile
-    # 'https://www.couchsurfing.com/people/damon-page' # no opening info
-    # 'https://www.couchsurfing.com/people/davor-medaric' # --reason 4 add code
-    # 'https://www.couchsurfing.com/people/mohcine-lar' # chatty-katy
-    # 'https://www.couchsurfing.com/people/davidsalib' # chatty-katy-> include
-    # 'https://www.couchsurfing.com/users/1005022110' # -- random Male
-    # 'https://www.couchsurfing.com/people/asmae-attif' # -- female
 
     host = []
     profile = {}
@@ -68,7 +61,8 @@ class Couchsurfing::Scraper
     host_name = doc.css('div.profile-sidebar__user-info span a span').text
     profile['name'] = host_name
 
-    z = doc.css('section:nth-child(3) div div div:nth-child(1) ul li:nth-child(3)').text
+    z = doc.css('section:nth-child(3) div div div:nth-child(1) ul ' \
+                 'li:nth-child(3)').text
     profile['age_sex'] = z
 
     about_me_section = doc.css('div:nth-child(5) section:nth-child(4) div')
@@ -76,26 +70,24 @@ class Couchsurfing::Scraper
     person = {}
     profile['about_me'] = person
 
-   if about_me_section.css('p:nth-child(2)').text == ''
-     person['About Me'] = about_me_section.css('p:nth-child(1)').text
-   else
-     person['About Me'] = about_me_section.css('p:nth-child(1)').text + ": " \
-                          + about_me_section.css('p:nth-child(2)').text unless \
-                          about_me_section.css('p:nth-child(1)').text == ''
-   end
+    if about_me_section.css('p:nth-child(2)').text == ''
+      person['About Me'] = about_me_section.css('p:nth-child(1)').text
+    else
+      person['About Me'] = about_me_section.css('p:nth-child(1)').text + ': ' + \
+                           about_me_section.css('p:nth-child(2)').text unless \
+                           about_me_section.css('p:nth-child(1)').text == ''
+    end
 
     about_me_section.search('h2').each do |node|
-      if (node.next_element.text).include?('COUCHSURFING')
-        person[node.text] = node.next_element.text + ': ' + node.next_element.next_element.text
+      if node.next_element.text.include?('COUCHSURFING')
+        person[node.text] = node.next_element.text + ': ' + \
+                            node.next_element.next_element.text
       else
-        person[node.text] = node.next_element.text if node.next_element.text != ''
+        person[node.text] = node.next_element.text \
+                           unless node.next_element.text == ''
       end
     end
     host << profile unless profile['name'] == ''
     host
   end
-
 end
-
- # x = Couchsurfing::Scraper.new
- # x.scrape_get_host_pages('asia', 'south-korea', 'busan')
